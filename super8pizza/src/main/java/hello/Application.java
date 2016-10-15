4c01f92ca1d717201750c2f8e11f4956e925424a
adampermann <adampermann@gmail.com>
2016-10-14 23:42:15 -0500

    Messing with the project to get Angular working
    Moved a bunch of stuff around.  Spring has a particular spot it likes static web content so had to change the structure a little. Going to add a landing page.

---
+++ super8pizza/src/main/java/hello/Application.java
@@ -0,0 +1,41 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by adampermann on 10/14/16.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
+
+}
